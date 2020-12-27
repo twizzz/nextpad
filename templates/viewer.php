212880442d6dd4062c2cd232f79c05caa815017d
Kennet Winter <kennet@kwinter.net>
2020-12-27 23:21:49 +0100

    v1.1.0

--- templates/viewer.php
+++ templates/viewer.php
@@ -13,7 +13,7 @@ $title = $_['title'];
   </head>
   <body style="margin: 0px; padding: 0px; overflow: hidden; bottom: 37px; top: 0px; left: 0px; right: 0px; position: absolute;">
     <div id="nextpad_bar">
-      <strong><?php p($title); ?></strong><span> - Direct link: <a target="_parent" href="<?php p($url); ?>"><?php p($url); ?></a></span><a id="nextpad_close">x</a>
+      <strong><?php p($title); ?></strong><span> - <a target="_parent" href="<?php p($url); ?>"><?php p($url); ?></a></span><a id="nextpad_close">x</a>
     </div>
     <iframe frameborder="0" id="nextpad_frame" style="overflow:hidden;width:100%;height:100%;display:block;" height="100%" width="100%" src="<?php p($url); ?>"></iframe>
   </body>
